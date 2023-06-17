3bc273452323840e67c1156ac3857034710609f6
Luka Matovic <lukamatovic@proton.me>
2023-06-17 12:25:00 +0200

    add: User Registration and Authentication
    * List of finished Key Components:
    User Registration: Users can provide necessary details such as username, email, and password to create a new account. The registration process typically includes validation of inputs and ensuring unique usernames or email addresses.
    
    Password Encryption: To enhance security, the user's password should be encrypted before storing it in the database. Techniques like bcrypt or PBKDF2 can be used for password hashing and salting.
    
    Authentication: After successful registration, users can log in using their credentials (username/email and password). The API verifies the provided information and generates a token that the user can use to authenticate subsequent requests.
    
    Token-Based Authentication: JSON Web Tokens (JWTs) are commonly used for token-based authentication. Upon successful authentication, the server generates a JWT containing user information and returns it to the client. The client includes this token in subsequent API requests in the authorization header to prove their identity.
    
    Token Validation: The API validates incoming JWTs to ensure their authenticity, integrity, and expiration. The server verifies the signature of the token using a secret key, checks its expiration date, and ensures it hasn't been tampered with.
    
    * Still to implement
    User Management: The API should provide functionality to manage user accounts, including features like updating user information, changing passwords, and deleting accounts.
    
    Password Reset: Implementing a password reset mechanism allows users to recover their accounts if they forget their passwords. This usually involves sending a password reset link or temporary password to the user's registered email address.
    
    Access Control: Certain API endpoints or actions might require different levels of access control (e.g., user roles or permissions). The API should enforce these access controls to ensure that only authorized users can perform specific operations.

---
+++ src/main/java/rs/lukamatovic/TaskMinder/TaskMinderApplication.java
@@ -0,0 +1,13 @@
+package rs.lukamatovic.TaskMinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMinderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/rs/lukamatovic/TaskMinder/model/ERole.java
@@ -0,0 +1,5 @@
+package rs.lukamatovic.TaskMinder.model;
+
+public enum ERole {
+	ROLE_USER
+}
